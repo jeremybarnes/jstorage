005cb728c5ba1ae80fe9ce5354e81194e7819518
jeremybarnes <jeremy@barneso.com>
2010-03-04 16:41:37 -0500

    persistent_versioned_object_test now compiling; tests still failing

--- mmap/testing/persistent_versioned_object_test.cc
+++ mmap/testing/persistent_versioned_object_test.cc
@@ -33,27 +33,10 @@ using namespace JMVCC;
 using namespace std;
 
 
-template<typename Pointer>
-struct MemoryRegion {
-    Pointer start;
-    size_t  length;
-};
-
 typedef uint64_t ObjectID;
 
 class PersistentObjectStore;
 
-
-/*****************************************************************************/
-/* AOTABLE                                                                   */
-/*****************************************************************************/
-
-/** A table of the addressable objects that are alive in a space.  Backed by
-    a memory map.  In an independent file so that it can grow independently
-    from the memory it uses.
-
-    For references to external objects, 
-*/
 
 
 
@@ -70,7 +53,7 @@ struct AddressableObject : public JMVCC::Versioned_Object {
     PersistentObjectStore * owner() const;
 
     /** How many versions of the object are there? */
-    virtual size_t num_versions() const;
+    virtual size_t num_versions() const = 0;
 
 private:
     ObjectID id_;  ///< Identity in the memory mapped region
@@ -79,15 +62,42 @@ private:
 
 
 /*****************************************************************************/
+/* AOTABLE                                                                   */
+/*****************************************************************************/
+
+/** A table of the addressable objects that are alive in a space.  Backed by
+    a memory map.  In an independent file so that it can grow independently
+    from the memory it uses.
+
+    For references to external objects, 
+*/
+
+// ...
+
+
+/*****************************************************************************/
 /* TYPED_AO                                                                  */
 /*****************************************************************************/
 
+/** Note that an addressable object potentially has multiple versions:
+    - Zero or one read-write version local to each sandbox;
+    - One or zero versions in the permanent storage;
+    - One or zero versions for each snapshot
+*/
+
 template<typename T>
-struct TypedAO : public AddressableObject {
+struct TypedAO
+    : public AddressableObject {
 
-    TypedAO();
+    TypedAO(const T & val = T())
+    {
+        version_table = VT::create(new T(val), 1);
+    }
 
-    TypedAO(const T & val);
+    ~TypedAO()
+    {
+        VT::free(const_cast<VT *>(vt()));
+    }
 
     // Client interface.  Just two methods to get at the current value.
     T & mutate()
@@ -96,11 +106,8 @@ struct TypedAO : public AddressableObject {
         T * local = current_trans->local_value<T>(this);
 
         if (!local) {
-            T value;
-            {
-                //value = get_data()->value_at_epoch(current_trans->epoch());
-            }
-            local = current_trans->local_value<T>(this, value);
+            T * value = value_at_epoch(current_trans->epoch());
+            local = current_trans->local_value<T>(this, *value);
             
             if (!local)
                 throw Exception("mutate(): no local was created");
@@ -114,88 +121,216 @@ struct TypedAO : public AddressableObject {
         mutate() = val;
     }
     
-    const T read() const
+    const T & read() const
     {
-        if (!current_trans) {
-            throw Exception("reading outside a transaction");
-        }
+        if (!current_trans) no_transaction_exception(this);
+
         const T * val = current_trans->local_value<T>(this);
         
         if (val) return *val;
-
-#if 0        
-        const Data * d = get_data();
-
-        T result = d->value_at_epoch(current_trans->epoch());
-        return result;
-#endif
-    }
-
-
-    // Get the commit ready and check that everything can go ahead, but
-    // don't actually perform the commit
-    virtual bool setup(Epoch old_epoch, Epoch new_epoch, void * data)
-    {
-        // 1.  Allocate persistent memory for the new value
         
-        // 2.  Allocate heap memory for the new internal data structure
+        const VT * d = vt();
 
-        // 3.  Success
-        throw Exception("not implemented");
+        const T * result = d->value_at_epoch(current_trans->epoch());
+        return *result;
     }
 
-    // Confirm a setup commit, making it permanent
+    size_t history_size() const
+    {
+        size_t result = vt()->size() - 1;
+        return result;
+    }
+
+    virtual size_t num_versions() const
+    {
+        return history_size();
+    }
+
+private:
+    const T * value_at_epoch(Epoch epoch) const
+    {
+        
+    }
+
+    // Internal version_table object allocated for when we have more than one
+    // version
+    typedef Version_Table<T *> VT;
+
+    // The single internal version_table member.  Updated atomically.
+    mutable VT * version_table;
+
+    const VT * vt() const
+    {
+        return reinterpret_cast<const VT *>(version_table);
+    }
+
+    bool set_version_table(const VT * & old_version_table,
+                           VT * new_version_table)
+    {
+        memory_barrier();
+
+        bool result = cmp_xchg(reinterpret_cast<VT * &>(version_table),
+                               const_cast<VT * &>(old_version_table),
+                               new_version_table);
+
+        if (!result) VT::free_now(new_version_table);
+        else VT::free(const_cast<VT *>(old_version_table));
+
+        return result;
+    }
+        
+public:
+    // Implement object interface
+
+    virtual bool setup(Epoch old_epoch, Epoch new_epoch, void * new_value)
+    {
+        for (;;) {
+            const VT * d = vt();
+
+            if (new_epoch != get_current_epoch() + 1)
+                throw Exception("epochs out of order");
+            
+            Epoch valid_from = 1;
+            if (d->size() > 1)
+                valid_from = d->element(d->size() - 2).valid_to;
+            
+            if (valid_from > old_epoch)
+                return false;  // something updated before us
+            
+            VT * new_version_table = d->copy(d->size() + 1);
+            new_version_table->back().valid_to = new_epoch;
+            new_version_table->push_back(1 /* valid_to */,
+                                         new T(*reinterpret_cast<T *>(new_value)));
+            
+            if (set_version_table(d, new_version_table)) return true;
+        }
+    }
+
     virtual void commit(Epoch new_epoch) throw ()
     {
-        // 1.  Serialize the new value in the new memory
+        const VT * d = vt();
 
-        // 2.  Switch the pointer to point to the new memory
+        // Now that it's definitive, we have an older entry to clean up
+        Epoch valid_from = 1;
+        if (d->size() > 2)
+            valid_from = d->element(d->size() - 3).valid_to;
 
-        // 3.  Arrange for the old memory to be freed once everything has made
-        //     a critical section transition
-        throw Exception("not implemented");
+        snapshot_info.register_cleanup(this, valid_from);
     }
 
-    // Roll back a setup commit
-    virtual void rollback(Epoch new_epoch, void * data) throw ()
+    virtual void rollback(Epoch new_epoch, void * local_version_table) throw ()
     {
-        // 1.  Deallocate the memory from the owner in the mmap range
-        throw Exception("not implemented");
+        const VT * d = vt();
+
+        for (;;) {
+            VT * d2 = d->copy(d->size());
+            d2->pop_back();
+            if (set_version_table(d, d2)) return;
+        }
     }
 
-    // Clean up an unused version
     virtual void cleanup(Epoch unused_valid_from, Epoch trigger_epoch)
     {
-        throw Exception("not implemented");
+        const VT * d = vt();
+
+        for (;;) {
+
+            if (d->size() < 2) {
+                using namespace std;
+                cerr << "cleaning up: unused_valid_from = " << unused_valid_from
+                     << " trigger_epoch = " << trigger_epoch << endl;
+                cerr << "current_epoch = " << get_current_epoch() << endl;
+                throw Exception("cleaning up with no values to clean up");
+            }
+
+            
+            VT * result = d->cleanup(unused_valid_from);
+            if (result) {
+                if (set_version_table(d, result)) return;
+                continue;
+            }
+            
+            static Lock lock;
+            Guard guard2(lock);
+            cerr << "----------- cleaning up didn't exist ---------" << endl;
+            dump_unlocked();
+            cerr << "unused_valid_from = " << unused_valid_from << endl;
+            cerr << "trigger_epoch = " << trigger_epoch << endl;
+            snapshot_info.dump();
+            cerr << "----------- end cleaning up didn't exist ---------" << endl;
+            
+            throw Exception("attempt to clean up something that didn't exist");
+        }
     }
     
-    // Rename an epoch to a different number.  Returns the valid_from value
-    // of the next epoch in the set.
-    virtual Epoch rename_epoch(Epoch old_valid_from,
-                               Epoch new_valid_from) throw ()
+    virtual Epoch rename_epoch(Epoch old_valid_from, Epoch new_valid_from)
+        throw ()
     {
-        throw Exception("not implemented");
+        const VT * d = vt();
+
+        for (;;) {
+            std::pair<VT *, Epoch> result
+                = d->rename_epoch(old_valid_from, new_valid_from);
+
+            if (!result.first)
+                throw Exception("not found");
+
+            if (set_version_table(d, result.first)) return result.second;
+        }
     }
 
     virtual void dump(std::ostream & stream = std::cerr, int indent = 0) const
     {
-        throw Exception("not implemented");
+        dump_itl(stream, indent);
     }
 
     virtual void dump_unlocked(std::ostream & stream = std::cerr,
                                int indent = 0) const
     {
-        throw Exception("not implemented");
+        dump_itl(stream, indent);
+    }
+
+    void dump_itl(std::ostream & stream, int indent = 0) const
+    {
+        const VT * d = vt();
+
+        using namespace std;
+        std::string s(indent, ' ');
+        stream << s << "object at " << this << std::endl;
+        stream << s << "history with " << d->size()
+               << " values" << endl;
+        for (unsigned i = 0;  i < d->size();  ++i) {
+            const typename VT::Entry & entry = d->element(i);
+            stream << s << "  " << i << ": valid to "
+                   << entry.valid_to;
+            stream << " addr " << &entry.value;
+            stream << " value " << entry.value;
+            stream << endl;
+        }
     }
 
     virtual std::string print_local_value(void * val) const
     {
-        throw Exception("not implemented");
+        return ostream_format(*reinterpret_cast<T *>(val));
     }
 
-private:
-    const char * current;  ///< Current version as serialized
+    virtual void destroy_local_value(void * val) const
+    {
+        reinterpret_cast<T *>(val)->~T();
+    }
 };
+
+
+/*****************************************************************************/
+/* AOREF                                                                     */
+/*****************************************************************************/
+
+/** This is a lightweight, temporary object designed to allow for access to
+    a value that may either
+    a) exist in memory as a sandboxed value;
+    b) exist in memory as a versioned object; or
+    c) be serialized on disk in some format.
+*/
 
 template<typename Obj, typename AO = TypedAO<Obj> >
 struct AORef {
